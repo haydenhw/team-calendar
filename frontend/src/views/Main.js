17920c51e80a0d88013351443fc8218008b0af6a
hayden7913 <hayden321@gmail.com>
2019-05-18 14:17:51 -0700

    menu toggle works in Main

--- frontend/src/views/Main.js
+++ frontend/src/views/Main.js
@@ -8,15 +8,36 @@ import {K_CIRCLE_SIZE, K_STICK_SIZE} from '../components/HoverMarkerStyles';
 import userMarkerImg from '../markers/user-marker.png';
 import chargeMarkerImg from '../markers/charge-marker.png';
 
-const ChargeMarker = ({ text }) => <img src={chargeMarkerImg}></img>;
+const styles = {
+        popup: {
+            position: "abosolute",
+            width: "100px",
+            height: "100px",
+            backgroundColor: "lightgrey",
+        }
+    }
+
+const ChargeMarker = ({ popupActive, handleClick }) => (
+  <div onClick={handleClick}>
+    {
+     popupActive 
+      ? <div style={styles.popup}/>
+      : <div/>
+    }
+    <img src={chargeMarkerImg}/>;
+  </div>
+);
+
 const UserMarker = ({ text }) => <img src={userMarkerImg}></img>;
 
 
 const hackathonLocation = { lat: 37.8039001, lng: -122.272983 };
-const testLocation = locations[0].coordinates;
-const testLocation2 = locations[1].coordinates;
 
 class SimpleMap extends Component {
+  state = {
+    open: false,
+  }
+
   static defaultProps = {
     center: hackathonLocation,
     zoom: 14,
@@ -28,23 +49,32 @@ class SimpleMap extends Component {
       const { data } = await axios(url);
       console.log(data);
     }
-    fetchData();
+    // fetchData();
   }
 
   renderChargeMarkers () {
     var chargeMarkers = locations.map((location, i) => {
 
       return (
-        <ChargeMarker key={i} lat={location.coordinates.lat} lng={location.coordinates.lng} />
+        <ChargeMarker popupActive={true} key={i} lat={location.coordinates.lat} lng={location.coordinates.lng} />
       );
     });
 
     return chargeMarkers;
   }
 
+  togglePopup = () => {
+    const { open } = this.state;
+
+    const logState = () => console.log(this.state);
+
+    this.setState({ open: !open }, logState);
+  }
+
   render () {
     return (
       // Important! Always set the container height explicitly
+
       <div style={{ height: '100vh', width: '100%' }}>
         <a href="http://www.google.com">Link to Today</a>
         <GoogleMapReact
@@ -54,8 +84,10 @@ class SimpleMap extends Component {
           hoverDistance={100 / 2}
 
         >
+          <ChargeMarker handleClick={this.togglePopup} popupActive={this.state.open} lat={hackathonLocation.lat} lng={hackathonLocation.lng} />
+
           {/* <UserMarker lat={hackathonLocation.lat} lng={hackathonLocation.lng} /> */}
-          <HoverMarker lat={hackathonLocation.lat} lng={hackathonLocation.lng} />
+          {/* <HoverMarker lat={hackathonLocation.lat} lng={hackathonLocation.lng} /> */}
           {/* {this.renderChargeMarkers()} */}
         </GoogleMapReact>
       </div>
