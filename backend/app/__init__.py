ed515fa029417ab83f86b6f01b326eab8b58482d
Akshay Dongaonkar <akshaydon93@gmail.com>
2019-05-18 12:59:15 -0700

    allow all the cors

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
-
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+CORS(app, resources={r"*": {"origins": "*"}})
+
 
 db = SQLAlchemy(app)
 db.app = app
